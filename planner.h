508c7fb72cbcf1c46d703d58f83b6d3d989e2d0c
Navinkumar Palanisamy <36514924+navinkumar357@users.noreply.github.com>
2020-08-05 17:07:11 +0200

    Create planner.h

---
+++ planner.h
@@ -0,0 +1,18 @@
+/**
+ * Program skeleton for the course "Programming embedded systems"
+ *
+ * Lab 1: the elevator control system
+ */
+
+/**
+ * The planner module, which is responsible for consuming
+ * pin/key events, and for deciding where the elevator
+ * should go next
+ */
+
+#ifndef PLANNER_H
+#define PLANNER_H
+
+void setupPlanner(unsigned portBASE_TYPE uxPriority);
+
+#endif
